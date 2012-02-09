f0edf7bc0b648e1f51ae61d6953c50d1e1f0b6ba
Roman Krejcik <farin@farin.cz>
2012-02-09 22:22:17 +0100

    "barn connection" scored in score phase

--- src/main/java/com/jcloisterzone/game/phase/ScorePhase.java
+++ src/main/java/com/jcloisterzone/game/phase/ScorePhase.java
@@ -1,20 +1,28 @@
 package com.jcloisterzone.game.phase;
 
 import java.util.Map.Entry;
+import java.util.Map;
 import java.util.Set;
 
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.jcloisterzone.Expansion;
+import com.jcloisterzone.Player;
 import com.jcloisterzone.PointCategory;
 import com.jcloisterzone.board.Location;
 import com.jcloisterzone.board.Position;
 import com.jcloisterzone.board.Tile;
 import com.jcloisterzone.feature.Castle;
+import com.jcloisterzone.feature.City;
 import com.jcloisterzone.feature.Cloister;
 import com.jcloisterzone.feature.Completable;
+import com.jcloisterzone.feature.Farm;
 import com.jcloisterzone.feature.Feature;
 import com.jcloisterzone.feature.Road;
+import com.jcloisterzone.feature.visitor.score.CityScoreContext;
 import com.jcloisterzone.feature.visitor.score.CompletableScoreContext;
+import com.jcloisterzone.feature.visitor.score.FarmScoreContext;
+import com.jcloisterzone.figure.Barn;
 import com.jcloisterzone.figure.Builder;
 import com.jcloisterzone.figure.Meeple;
 import com.jcloisterzone.game.Game;
@@ -45,10 +53,45 @@ public class ScorePhase extends Phase {
 			}
 		}
 	}
+	
+	private void scoreFollowersOnBarnFarm(Farm farm, Map<City, CityScoreContext> cityCache) {
+		FarmScoreContext ctx = farm.getScoreContext();
+		ctx.setCityCache(cityCache);
+		farm.walk(ctx);
+
+		boolean hasBarn = false;
+		for(Meeple m : ctx.getSpecialMeeples()) {
+			if (m instanceof Barn) {
+				hasBarn = true;
+				break;
+			}
+		}
+		if (hasBarn) {
+			for(Player p : ctx.getMajorOwners()) {
+				int points = ctx.getPointsWhenBarnIsConnected(p);
+				game.scoreFeature(points, ctx, p);
+			}
+			for(Meeple m : ctx.getMeeples()) {
+				if (! (m instanceof Barn)) {
+					m.undeploy(false);
+				}
+			}
+		}
+	}
 
 	@Override
 	public void enter() {
 		Position pos = getTile().getPosition();
+		
+		//TODO separate event here ??? and move this code to abbey and mayor game
+		if (game.hasExpansion(Expansion.ABBEY_AND_MAYOR)) {
+			Map<City, CityScoreContext> cityCache = Maps.newHashMap();
+			for(Feature feature : getTile().getFeatures()) {
+				if (feature instanceof Farm) {
+					scoreFollowersOnBarnFarm((Farm) feature, cityCache);
+				}
+			}
+		}		
 
 		scoreCompletedOnTile(getTile());
 		if (getTile().isAbbeyTile()) {
--- src/main/java/com/jcloisterzone/game/phase/TilePhase.java
+++ src/main/java/com/jcloisterzone/game/phase/TilePhase.java
@@ -5,24 +5,15 @@ import java.util.Set;
 
 import com.google.common.collect.Maps;
 import com.jcloisterzone.Expansion;
-import com.jcloisterzone.Player;
 import com.jcloisterzone.board.Location;
 import com.jcloisterzone.board.Position;
 import com.jcloisterzone.board.Rotation;
 import com.jcloisterzone.board.Tile;
 import com.jcloisterzone.collection.Sites;
-import com.jcloisterzone.feature.City;
-import com.jcloisterzone.feature.Farm;
-import com.jcloisterzone.feature.Feature;
-import com.jcloisterzone.feature.visitor.score.CityScoreContext;
-import com.jcloisterzone.feature.visitor.score.FarmScoreContext;
-import com.jcloisterzone.figure.Barn;
-import com.jcloisterzone.figure.Meeple;
 import com.jcloisterzone.game.Game;
 import com.jcloisterzone.game.expansion.BridgesCastlesBazaarsGame;
 
 public class TilePhase extends Phase {
-
 
 	public TilePhase(Game game) {
 		super(game);
@@ -32,31 +23,6 @@ public class TilePhase extends Phase {
 	public void enter() {
 		Map<Position, Set<Rotation>> freezed = Maps.newHashMap(getBoard().getAvailablePlacements());
 		game.getUserInterface().selectTilePlacement(freezed);
-	}
-
-	private void scoreFollowersOnBarnFarm(Farm farm, Map<City, CityScoreContext> cityCache) {
-		FarmScoreContext ctx = farm.getScoreContext();
-		ctx.setCityCache(cityCache);
-		farm.walk(ctx);
-
-		boolean hasBarn = false;
-		for(Meeple m : ctx.getSpecialMeeples()) {
-			if (m instanceof Barn) {
-				hasBarn = true;
-				break;
-			}
-		}
-		if (hasBarn) {
-			for(Player p : ctx.getMajorOwners()) {
-				int points = ctx.getPointsWhenBarnIsConnected(p);
-				game.scoreFeature(points, ctx, p);
-			}
-			for(Meeple m : ctx.getMeeples()) {
-				if (! (m instanceof Barn)) {
-					m.undeploy(false);
-				}
-			}
-		}
 	}
 
 	@Override
@@ -86,20 +52,8 @@ public class TilePhase extends Phase {
 			bcb.decreaseBridges(getActivePlayer());		
 			bcb.deployBridge(pos, loc);
 		}
-		getBoard().mergeFeatures(tile);
-		
-		//TODO seperate tileMerged event here ??? and move this code to abbey and mayor game
-		if (game.hasExpansion(Expansion.ABBEY_AND_MAYOR)) {
-			Map<City, CityScoreContext> cityCache = Maps.newHashMap();
-			for(Feature feature : getTile().getFeatures()) {
-				if (feature instanceof Farm) {
-					scoreFollowersOnBarnFarm((Farm) feature, cityCache);
-				}
-			}
-		}
+		getBoard().mergeFeatures(tile);		
 		
 		next();
 	}
-
-
 }
