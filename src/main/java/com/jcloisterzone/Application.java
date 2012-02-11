168392f8bf6ffb829815e3339fb7515c3386e109
Roman Krejcik <farin@farin.cz>
2012-02-11 13:59:51 +0100

    2.0.4

--- src/main/java/com/jcloisterzone/Application.java
+++ src/main/java/com/jcloisterzone/Application.java
@@ -9,7 +9,7 @@ public interface Application {
 	public String VERSION = "DEV";
 	public String BUILD_DATE = "2012-00-00";
 
-	public int PROTCOL_VERSION = 7;
+	public int PROTCOL_VERSION = 8;
 
 	public static final String ILLEGAL_STATE_MSG = "Method '{}' called in invalid state";
 }
