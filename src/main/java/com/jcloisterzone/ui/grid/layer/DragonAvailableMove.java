bd8e0bfa0a7b0017eb63ed2f25fc31c0b60a2962
Roman Krejcik <farin@farin.cz>
2012-06-09 16:52:38 +0200

    fix: do not call remote dragon move on each layer click

--- src/main/java/com/jcloisterzone/ui/grid/layer/DragonAvailableMove.java
+++ src/main/java/com/jcloisterzone/ui/grid/layer/DragonAvailableMove.java
@@ -12,43 +12,45 @@ import com.jcloisterzone.ui.grid.GridPanel;
 
 public class DragonAvailableMove extends AbstractGridLayer implements GridMouseListener {
 
-	private Set<Position> positions;
-	private Position selected;
+    private Set<Position> positions;
+    private Position selected;
 
-	public DragonAvailableMove(GridPanel gridPanel, Set<Position> positions) {
-		super(gridPanel);
-		this.positions = positions;
-	}
+    public DragonAvailableMove(GridPanel gridPanel, Set<Position> positions) {
+        super(gridPanel);
+        this.positions = positions;
+    }
 
-	public void paint(Graphics2D g2) {
-		Image dragon = getClient().getControlsTheme().getActionDecoration("dragon");
-		for(Position pos : positions) {
-			g2.drawImage(dragon, getOffsetX(pos), getOffsetY(pos), getSquareSize(), getSquareSize(), null);
-		}
-	}
+    public void paint(Graphics2D g2) {
+        Image dragon = getClient().getControlsTheme().getActionDecoration("dragon");
+        for(Position pos : positions) {
+            g2.drawImage(dragon, getOffsetX(pos), getOffsetY(pos), getSquareSize(), getSquareSize(), null);
+        }
+    }
 
-	@Override
-	public int getZIndex() {
-		return 60;
-	}
+    @Override
+    public int getZIndex() {
+        return 60;
+    }
 
-	@Override
-	public void squareEntered(MouseEvent e, Position p) {
-		selected = p;
-	}
+    @Override
+    public void squareEntered(MouseEvent e, Position p) {
+        selected = p;
+    }
 
-	@Override
-	public void squareExited(MouseEvent e, Position p) {
-		selected = null;
+    @Override
+    public void squareExited(MouseEvent e, Position p) {
+        selected = null;
 
-	}
+    }
 
-	@Override
-	public void mouseClicked(MouseEvent e, Position p) {
-		if (e.getButton() == MouseEvent.BUTTON1) {
-			getClient().getServer().moveDragon(selected);
-		}
-	}
+    @Override
+    public void mouseClicked(MouseEvent e, Position p) {
+        if (e.getButton() == MouseEvent.BUTTON1) {
+            if (positions.contains(selected)) {
+                getClient().getServer().moveDragon(selected);
+            }
+        }
+    }
 
 
 }
