02194656cf417a116443fd94ab8d3834672d098b
Roman Krejcik <farin@farin.cz>
2012-08-27 17:03:07 +0200

    fix: multiplied barn score when two or more barns are on same farm

--- src/main/java/com/jcloisterzone/feature/score/ScoreAllFeatureFinder.java
+++ src/main/java/com/jcloisterzone/feature/score/ScoreAllFeatureFinder.java
@@ -30,86 +30,87 @@ import com.jcloisterzone.game.Game;
  */
 public class ScoreAllFeatureFinder {
 
-	private Set<Follower> alreadyRated = Sets.newHashSet();
-	private List<FarmScoreContext> farmContexts = Lists.newArrayList();
-	private Map<City, CityScoreContext> cityCache = Maps.newHashMap();
-	private Map<Player, Set<City>> scoredCities;
+    private Set<Meeple> alreadyRated = Sets.newHashSet();
+    private List<FarmScoreContext> farmContexts = Lists.newArrayList();
+    private Map<City, CityScoreContext> cityCache = Maps.newHashMap();
+    private Map<Player, Set<City>> scoredCities;
 
-	public void scoreAll(Game game, ScoreAllCallback callback) {
-		if (game.hasRule(CustomRule.FARM_CITY_SCORED_ONCE)) {
-			scoredCities = Maps.newHashMap();
-			for(Player p : game.getAllPlayers()) {
-				scoredCities.put(p, Sets.<City>newHashSet());
-			}
-		}
-		for(Meeple m : game.getDeployedMeeples()) {
-			if (!(m instanceof Follower)) continue;
-			Feature f = m.getFeature();				
-			
-			if (f instanceof Castle) {
-				callback.scoreCastle(m, (Castle) f);
-				continue;
-			}
-			
-			if (!(f instanceof Completable)) continue;
-			if (alreadyRated.contains(m)) continue;	
-			
-			Completable completable = (Completable) f;
-			CompletableScoreContext ctx = callback.getCompletableScoreContext(completable);
-			if (ctx instanceof CityScoreContext) {
-				((CityScoreContext) ctx).setCityCache(cityCache);
-			}
-			completable.walk(ctx);
-			alreadyRated.addAll(ctx.getFollowers());
-			callback.scoreCompletableFeature(ctx);
-		}
-		for(Meeple m : game.getDeployedMeeples()) {
-			if (! (m instanceof Follower) && !(m instanceof Barn)) continue;
-			Feature f = m.getFeature();
-			if (! (f instanceof Farm)) continue;
-			if (alreadyRated.contains(m)) continue;
+    public void scoreAll(Game game, ScoreAllCallback callback) {
+        if (game.hasRule(CustomRule.FARM_CITY_SCORED_ONCE)) {
+            scoredCities = Maps.newHashMap();
+            for(Player p : game.getAllPlayers()) {
+                scoredCities.put(p, Sets.<City>newHashSet());
+            }
+        }
+        for(Meeple m : game.getDeployedMeeples()) {
+            if (!(m instanceof Follower)) continue;
+            Feature f = m.getFeature();
 
-			Farm farm = (Farm) f;
-			FarmScoreContext ctx = callback.getFarmScoreContext(farm);
-			ctx.setCityCache(cityCache);
-			if (scoredCities != null) {
-				ctx.setScoredCities(scoredCities);
-			}
-			farm.walk(ctx);
-			alreadyRated.addAll(ctx.getFollowers());
-			farmContexts.add(ctx);
-		}
-		for(Player p : game.getAllPlayers()) {
-			Collections.sort(farmContexts, new FarmPoitsPerCityComparator(p));
-			for(FarmScoreContext ctx : farmContexts) {
-				if (ctx.getMajorOwners().contains(p)) {
-					callback.scoreFarm(ctx, p);
-				}
-			}
-		}
-		for(FarmScoreContext ctx : farmContexts) {
-			for(Special m : ctx.getSpecialMeeples()) {
-				if (m instanceof Barn) {
-					callback.scoreBarn(ctx, (Barn) m);
-				}
-			}
-		}
-	}
+            if (f instanceof Castle) {
+                callback.scoreCastle(m, (Castle) f);
+                continue;
+            }
 
-	private static class FarmPoitsPerCityComparator implements Comparator<FarmScoreContext> {
+            if (!(f instanceof Completable)) continue;
+            if (alreadyRated.contains(m)) continue;
 
-		private final Player player;
+            Completable completable = (Completable) f;
+            CompletableScoreContext ctx = callback.getCompletableScoreContext(completable);
+            if (ctx instanceof CityScoreContext) {
+                ((CityScoreContext) ctx).setCityCache(cityCache);
+            }
+            completable.walk(ctx);
+            alreadyRated.addAll(ctx.getFollowers());
+            callback.scoreCompletableFeature(ctx);
+        }
+        for(Meeple m : game.getDeployedMeeples()) {
+            if (! (m instanceof Follower) && !(m instanceof Barn)) continue;
+            Feature f = m.getFeature();
+            if (! (f instanceof Farm)) continue;
+            if (alreadyRated.contains(m)) continue;
 
-		public FarmPoitsPerCityComparator(Player player) {
-			this.player = player;
-		}
+            Farm farm = (Farm) f;
+            FarmScoreContext ctx = callback.getFarmScoreContext(farm);
+            ctx.setCityCache(cityCache);
+            if (scoredCities != null) {
+                ctx.setScoredCities(scoredCities);
+            }
+            farm.walk(ctx);
+            alreadyRated.addAll(ctx.getFollowers());
+            alreadyRated.addAll(ctx.getSpecialMeeples()); //farm can contains Barn!
+            farmContexts.add(ctx);
+        }
 
-		@Override
-		public int compare(FarmScoreContext o1, FarmScoreContext o2) {
-			//reverse order according to city points
-			return o2.getPointsPerCity(player) - o1.getPointsPerCity(player);
-		}
+        for(Player p : game.getAllPlayers()) {
+            //2nd edition farm scoring (CustomRule.FARM_CITY_SCORED_ONCE) requires sort to assign correct (highest) points
+            Collections.sort(farmContexts, new FarmPoitsPerCityComparator(p));
+            for(FarmScoreContext ctx : farmContexts) {
+                if (ctx.getMajorOwners().contains(p)) {
+                    callback.scoreFarm(ctx, p);
+                }
+            }
+        }
+        for(FarmScoreContext ctx : farmContexts) {
+            for(Special m : ctx.getSpecialMeeples()) {
+                if (m instanceof Barn) {
+                    callback.scoreBarn(ctx, (Barn) m);
+                }
+            }
+        }
+    }
 
-	}
+    private static class FarmPoitsPerCityComparator implements Comparator<FarmScoreContext> {
 
+        private final Player player;
+
+        public FarmPoitsPerCityComparator(Player player) {
+            this.player = player;
+        }
+
+        @Override
+        public int compare(FarmScoreContext o1, FarmScoreContext o2) {
+            //reverse order according to city points
+            return o2.getPointsPerCity(player) - o1.getPointsPerCity(player);
+        }
+    }
 }
