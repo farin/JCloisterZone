e0ae2329179e58ef2d4cf72969ce894eed80bc98
Roman Krejcik <farin@farin.cz>
2013-08-16 23:19:23 +0200

    catapult: tile clarification, fete split farm

--- src/main/java/com/jcloisterzone/Application.java
+++ src/main/java/com/jcloisterzone/Application.java
@@ -6,10 +6,10 @@ package com.jcloisterzone;
  */
 public interface Application {
 
-    public String VERSION = "2.2";
-    public String BUILD_DATE = "2013-05-09";
+    public String VERSION = "dev";
+    public String BUILD_DATE = "YYYY-MM-DD";
 
-    public int PROTCOL_VERSION = 12;
+    public int PROTCOL_VERSION = 13;
 
     public static final String ILLEGAL_STATE_MSG = "Method '{}' called in invalid state";
 }
