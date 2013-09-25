67f2f498374aca4ed7ce8063ff8b4217979637ad
Roman Krejcik <farin@farin.cz>
2013-09-25 11:29:51 +0200

    tests fixed

--- src/test/java/com/jcloisterzone/feature/AbstractScoringTest.java
+++ src/test/java/com/jcloisterzone/feature/AbstractScoringTest.java
@@ -1,5 +1,7 @@
 package com.jcloisterzone.feature;
 
+import java.util.Arrays;
+
 import org.junit.After;
 import org.junit.Before;
 
@@ -11,32 +13,36 @@ import com.jcloisterzone.board.Tile;
 import com.jcloisterzone.game.Game;
 
 public class AbstractScoringTest extends AbstractTileTest {
-	
-	protected void setUpGame(Game game) {
-		game.getExpansions().add(Expansion.BASIC);
-		game.getExpansions().add(Expansion.INNS_AND_CATHEDRALS);
-		game.getExpansions().add(Expansion.TRADERS_AND_BUILDERS);
-		game.getExpansions().add(Expansion.ABBEY_AND_MAYOR);
-		game.getExpansions().add(Expansion.CATHARS);
-	}
-	
-	@Before
-	public void setUp() {
-		game.start();
-	}
-	
-	@After
-	public void tearDown() {
-		game.getCustomRules().clear();
-	}
-	
-	protected Tile putTile(Position pos, Rotation rot,  Expansion exp, String id) {
-		Tile tile = createTile(exp, id);
-		tile.setRotation(rot);
-		game.getBoard().refreshAvailablePlacements(tile);
-		game.getBoard().add(tile, pos, true);
-		game.getBoard().mergeFeatures(tile);
-		return tile;
-	}
-	
+
+    protected void setUpGame(Game game) {
+        game.getExpansions().add(Expansion.BASIC);
+        game.getExpansions().add(Expansion.INNS_AND_CATHEDRALS);
+        game.getExpansions().add(Expansion.TRADERS_AND_BUILDERS);
+        game.getExpansions().add(Expansion.ABBEY_AND_MAYOR);
+        game.getExpansions().add(Expansion.CATHARS);
+
+        for (Expansion exp : game.getExpansions()) {
+            game.getCapabilities().addAll(Arrays.asList(exp.getCapabilities()));
+        }
+    }
+
+    @Before
+    public void setUp() {
+        game.start();
+    }
+
+    @After
+    public void tearDown() {
+        game.getCustomRules().clear();
+    }
+
+    protected Tile putTile(Position pos, Rotation rot,  Expansion exp, String id) {
+        Tile tile = createTile(exp, id);
+        tile.setRotation(rot);
+        game.getBoard().refreshAvailablePlacements(tile);
+        game.getBoard().add(tile, pos, true);
+        game.getBoard().mergeFeatures(tile);
+        return tile;
+    }
+
 }
--- src/test/java/com/jcloisterzone/feature/RoadScoring.java
+++ src/test/java/com/jcloisterzone/feature/RoadScoring.java
@@ -13,77 +13,77 @@ import com.jcloisterzone.feature.visitor.score.RoadScoreContext;
 
 public class RoadScoring extends AbstractScoringTest {
 
-	protected void assertScore(int expected, Tile tile, Location loc) {
-		Road road = (Road) tile.getFeaturePartOf(Location.W);
-		RoadScoreContext ctx = road.getScoreContext();
-		road.walk(ctx);
+    protected void assertScore(int expected, Tile tile, Location loc) {
+        Road road = (Road) tile.getFeaturePartOf(Location.W);
+        RoadScoreContext ctx = road.getScoreContext();
+        road.walk(ctx);
 
-		assertEquals(expected, ctx.getPoints());
-	}
+        assertEquals(expected, ctx.getPoints());
+    }
 
-	@Test
-	public void simple() {
-		Tile t;
-		t = putTile(new Position(0,0), Rotation.R0, Expansion.BASIC, "RCr");
-		putTile(new Position(1,0), Rotation.R0, Expansion.BASIC, "RFr");
-		putTile(new Position(-1,0), Rotation.R0, Expansion.BASIC, "RRRR");
-		putTile(new Position(2,0), Rotation.R90, Expansion.BASIC, "LR");
+    @Test
+    public void simple() {
+        Tile t;
+        t = putTile(new Position(0,0), Rotation.R0, Expansion.BASIC, "RCr");
+        putTile(new Position(1,0), Rotation.R0, Expansion.BASIC, "RFr");
+        putTile(new Position(-1,0), Rotation.R0, Expansion.BASIC, "RRRR");
+        putTile(new Position(2,0), Rotation.R90, Expansion.BASIC, "LR");
 
-		assertScore(4, t, Location.W);
-	}
+        assertScore(4, t, Location.W);
+    }
 
-	@Test
-	public void circle() {
-		Tile t;
-		t = putTile(new Position(0,0), Rotation.R0, Expansion.BASIC, "Rr");
-		putTile(new Position(-1,0), Rotation.R270, Expansion.BASIC, "Rr");
-		putTile(new Position(0,1), Rotation.R90, Expansion.BASIC, "RrC");
-		putTile(new Position(-1,1), Rotation.R270, Expansion.BASIC, "CcRr");
+    @Test
+    public void circle() {
+        Tile t;
+        t = putTile(new Position(0,0), Rotation.R0, Expansion.BASIC, "Rr");
+        putTile(new Position(-1,0), Rotation.R270, Expansion.BASIC, "Rr");
+        putTile(new Position(0,1), Rotation.R90, Expansion.BASIC, "RrC");
+        putTile(new Position(-1,1), Rotation.R270, Expansion.BASIC, "CcRr");
 
-		assertScore(4, t, Location.W);
-	}
+        assertScore(4, t, Location.W);
+    }
 
-	@Test
-	public void circleWithCross() {
-		Tile t;
-		t = putTile(new Position(0,0), Rotation.R0, Expansion.BASIC, "Rr");
-		putTile(new Position(-1,0), Rotation.R270, Expansion.BASIC, "Rr");
-		putTile(new Position(0,1), Rotation.R90, Expansion.BASIC, "RrC");
-		putTile(new Position(-1,1), Rotation.R0, Expansion.BASIC, "RRRR");
+    @Test
+    public void circleWithCross() {
+        Tile t;
+        t = putTile(new Position(0,0), Rotation.R0, Expansion.BASIC, "Rr");
+        putTile(new Position(-1,0), Rotation.R270, Expansion.BASIC, "Rr");
+        putTile(new Position(0,1), Rotation.R90, Expansion.BASIC, "RrC");
+        putTile(new Position(-1,1), Rotation.R0, Expansion.BASIC, "RRRR");
 
-		assertScore(4, t, Location.W);
-	}
+        assertScore(4, t, Location.W);
+    }
 
-	@Test
-	public void inn() {
-		Tile t;
-		t = putTile(new Position(0,0), Rotation.R0, Expansion.BASIC, "RCr");
-		putTile(new Position(-1,0), Rotation.R0, Expansion.BASIC, "RRRR");
-		putTile(new Position(1,0), Rotation.R0, Expansion.INNS_AND_CATHEDRALS, "RFr.i");
-		putTile(new Position(2,0), Rotation.R90, Expansion.BASIC, "LR");
+    @Test
+    public void inn() {
+        Tile t;
+        t = putTile(new Position(0,0), Rotation.R0, Expansion.BASIC, "RCr");
+        putTile(new Position(-1,0), Rotation.R0, Expansion.BASIC, "RRRR");
+        putTile(new Position(1,0), Rotation.R0, Expansion.INNS_AND_CATHEDRALS, "RFr.i");
+        putTile(new Position(2,0), Rotation.R90, Expansion.BASIC, "LR");
 
-		assertScore(8, t, Location.W);
-	}
+        assertScore(8, t, Location.W);
+    }
 
-	@Test
-	public void unfinishedInn() {
-		Tile t;
-		t = putTile(new Position(0,0), Rotation.R0, Expansion.BASIC, "RCr");
-		putTile(new Position(-1,0), Rotation.R0, Expansion.BASIC, "RRRR");
-		putTile(new Position(1,0), Rotation.R0, Expansion.INNS_AND_CATHEDRALS, "RFr.i");
+    @Test
+    public void unfinishedInn() {
+        Tile t;
+        t = putTile(new Position(0,0), Rotation.R0, Expansion.BASIC, "RCr");
+        putTile(new Position(-1,0), Rotation.R0, Expansion.BASIC, "RRRR");
+        putTile(new Position(1,0), Rotation.R0, Expansion.INNS_AND_CATHEDRALS, "RFr.i");
 
-		assertScore(0, t, Location.W);
-	}
+        assertScore(0, t, Location.W);
+    }
 
-	@Test
-	public void well() {
-		Tile t;
-		t = putTile(new Position(0,0), Rotation.R0, Expansion.ABBEY_AND_MAYOR, "RRR");
-		putTile(new Position(0,-1), Rotation.R0, Expansion.ABBEY_AND_MAYOR, "R");
-		putTile(new Position(-1, 0), Rotation.R270, Expansion.ABBEY_AND_MAYOR, "R");
-		putTile(new Position(0, 1), Rotation.R180, Expansion.ABBEY_AND_MAYOR, "R");
+    @Test
+    public void well() {
+        Tile t;
+        t = putTile(new Position(0,0), Rotation.R0, Expansion.ABBEY_AND_MAYOR, "Rrr");
+        putTile(new Position(0,-1), Rotation.R0, Expansion.ABBEY_AND_MAYOR, "R");
+        putTile(new Position(-1, 0), Rotation.R270, Expansion.ABBEY_AND_MAYOR, "R");
+        putTile(new Position(1, 0), Rotation.R90, Expansion.ABBEY_AND_MAYOR, "R");
 
-		assertScore(4, t, Location.W);
-	}
+        assertScore(4, t, Location.W);
+    }
 
 }
