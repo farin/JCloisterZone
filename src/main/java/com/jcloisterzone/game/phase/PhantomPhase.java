48d1b9e433fe4ef132b97d25b06c355e6f75e3b3
Roman Krejcik <farin@farin.cz>
2014-08-20 23:12:49 +0200

    removed unused method

--- src/main/java/com/jcloisterzone/game/phase/PhantomPhase.java
+++ src/main/java/com/jcloisterzone/game/phase/PhantomPhase.java
@@ -37,6 +37,7 @@ public class PhantomPhase extends Phase {
 
     @Override
     public void enter() {
+        //TODO what about pay ransom for phantom now?
         if (!getActivePlayer().hasFollower(Phantom.class)) {
             next();
             return;
@@ -55,16 +56,6 @@ public class PhantomPhase extends Phase {
         } else {
             game.post(new SelectActionEvent(getActivePlayer(), actions, true));
         }
-    }
-
-    //TODO copy from Action phase -> merge
-    private boolean isAutoTurnEnd(List<? extends PlayerAction<?>> actions) {
-        if (!actions.isEmpty()) return false;
-        if (towerCap != null && !towerCap.isRansomPaidThisTurn() && towerCap.hasImprisonedFollower(getActivePlayer(), Phantom.class)) {
-            //player can return phantom figure immediately
-            return false;
-        }
-        return true;
     }
 
     @Override
