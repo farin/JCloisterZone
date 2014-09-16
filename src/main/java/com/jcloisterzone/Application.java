035f5b6711fd3310cee35e891105480ea7ac9fe7
Roman Krejcik <farin@farin.cz>
2014-09-16 22:14:27 +0200

    3.0.1 - with bugreport form

--- src/main/java/com/jcloisterzone/Application.java
+++ src/main/java/com/jcloisterzone/Application.java
@@ -8,8 +8,8 @@ public interface Application {
 
     public String DEV_VERSION = "dev-snapshot";
 
-    public String VERSION = "3.0";
-    public String BUILD_DATE = "2014-08-27";
+    public String VERSION = "3.0.1";
+    public String BUILD_DATE = "2014-09-15";
 
     public String PROTCOL_VERSION = "3.0";
 
