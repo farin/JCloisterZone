d4485d9a3045914d2231eb29dc8a6f52b9367404
Roman Krejcik <farin@farin.cz>
2014-11-02 20:25:21 +0100

    release 3.0.2
    * rules update: taking prisoner is now not mandatory
    * rules update: The placement of a princess tile (Princess & Dragon)
    with removal of a knight
       from the city cannot be used as a first “follower move” and be
    followed by placement
       of the Phantom (e.g. into the now-vacated city). As per the rules for
    the princess,
        “if a knight is removed from the city, the player may not deploy or
    move any other figure.”
    * fix: abbot tower capture
    * fix: wrong nickname in pre-game chat

--- src/main/java/com/jcloisterzone/Application.java
+++ src/main/java/com/jcloisterzone/Application.java
@@ -8,8 +8,8 @@ public interface Application {
 
     public String DEV_VERSION = "dev-snapshot";
 
-    public String VERSION = "3.0.1";
-    public String BUILD_DATE = "2014-09-17";
+    public String VERSION = "3.0.2";
+    public String BUILD_DATE = "2014-11-02";
 
     public String PROTCOL_VERSION = "3.0.2";
 
