ad2976a2fbfd88aa24a4246ca2062fb95c7a4200
Roman Krejcik <farin@farin.cz>
2015-07-18 21:55:59 +0200

    releae 3.3.0-beta

--- src/main/java/com/jcloisterzone/Application.java
+++ src/main/java/com/jcloisterzone/Application.java
@@ -8,10 +8,10 @@ public interface Application {
 
     public String DEV_VERSION = "dev-snapshot";
 
-    public String VERSION = "3.2.0";
-    public String BUILD_DATE = "2015-04-26";
+    public String VERSION = "3.3.0-beta";
+    public String BUILD_DATE = "2015-07-18";
 
-    public String PROTCOL_VERSION = "3.2.0";
+    public String PROTCOL_VERSION = "3.2.99"; //means beta
 
     public static final String ILLEGAL_STATE_MSG = "Method '{}' called in invalid state";
 }
