97460b23de950de0270e57ecf2591ff1f403c760
Roman Krejcik <farin@farin.cz>
2015-08-02 13:40:53 +0200

    release 3.3.0-beta2

--- src/main/java/com/jcloisterzone/Application.java
+++ src/main/java/com/jcloisterzone/Application.java
@@ -8,8 +8,8 @@ public interface Application {
 
     public String DEV_VERSION = "dev-snapshot";
 
-    public String VERSION = "3.3.0-beta";
-    public String BUILD_DATE = "2015-07-18";
+    public String VERSION = "3.3.0-beta2";
+    public String BUILD_DATE = "2015-08-02";
 
     public String PROTCOL_VERSION = "3.2.99"; //means beta
 
