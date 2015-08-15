3110b87ecf99b99f3baa10eb8ed3aa3c400469a6
Roman Krejcik <farin@farin.cz>
2015-08-15 09:27:29 +0200

    appupdate suffixes

--- src/main/java/com/jcloisterzone/Application.java
+++ src/main/java/com/jcloisterzone/Application.java
@@ -14,7 +14,7 @@ public interface Application {
     public String VERSION = DEV_VERSION;
     public String BUILD_DATE = "";
 
-    public String PROTCOL_VERSION = "3.2.0";
+    public String PROTCOL_VERSION = "3.3.0";
 
     public static final String ILLEGAL_STATE_MSG = "Method '{}' called in invalid state";
 }
