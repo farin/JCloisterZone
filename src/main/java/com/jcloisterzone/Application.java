2b9d3bd00bc6672df9f9d8ee7d1db7829da679f0
Roman Krejcik <farin@farin.cz>
2015-08-25 10:18:09 +0200

    release 3.3.1

--- src/main/java/com/jcloisterzone/Application.java
+++ src/main/java/com/jcloisterzone/Application.java
@@ -8,8 +8,8 @@ public interface Application {
 
     public String DEV_VERSION = "dev-snapshot";
 
-    public String VERSION = "3.3.0";
-    public String BUILD_DATE = "2015-08-15";
+    public String VERSION = "3.3.1";
+    public String BUILD_DATE = "2015-08-25";
 
     public String PROTCOL_VERSION = "3.3.1";
 
