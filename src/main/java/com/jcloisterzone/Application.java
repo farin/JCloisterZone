d3451405667c88a321f63e1b27c7f505aac2a36c
Roman Krejcik <farin@farin.cz>
2015-10-15 21:59:54 +0200

    release 3.4.0

--- src/main/java/com/jcloisterzone/Application.java
+++ src/main/java/com/jcloisterzone/Application.java
@@ -8,8 +8,8 @@ public interface Application {
 
     public String DEV_VERSION = "dev-snapshot";
 
-    public String VERSION = "3.3.1";
-    public String BUILD_DATE = "2015-08-25";
+    public String VERSION = "3.4.0";
+    public String BUILD_DATE = "2015-10-15";
 
     public String PROTCOL_VERSION = "3.4.0";
 
