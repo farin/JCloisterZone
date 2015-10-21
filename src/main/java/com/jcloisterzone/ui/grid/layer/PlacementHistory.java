8fa3eade7c2e14bbc11a951497bd46d6aa19557e
fatsu <tom.goemaes@gmail.com>
2015-10-22 00:29:44 +0200

    Placement History pimping
    1. Show placement history in player colors
    2. Show all placements since current player last turn.

--- src/main/java/com/jcloisterzone/ui/grid/layer/PlacementHistory.java
+++ src/main/java/com/jcloisterzone/ui/grid/layer/PlacementHistory.java
@@ -1,49 +1,132 @@
 package com.jcloisterzone.ui.grid.layer;
 
+import java.awt.AlphaComposite;
 import java.awt.Color;
+import java.awt.Composite;
 import java.awt.Graphics2D;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.util.LinkedList;
+import java.util.List;
 
+import com.google.common.eventbus.Subscribe;
+import com.jcloisterzone.Player;
 import com.jcloisterzone.board.Position;
-import com.jcloisterzone.board.Tile;
+import com.jcloisterzone.event.TileEvent;
 import com.jcloisterzone.ui.GameController;
 import com.jcloisterzone.ui.ImmutablePoint;
 import com.jcloisterzone.ui.grid.GridPanel;
 
 public class PlacementHistory extends AbstractGridLayer {
 
-    private static final Color COLOR = new Color(0,0,0,128);
+    private static final Color DEFAULT_COLOR = new Color(0,0,0,128);
     private static final ImmutablePoint POINT = new ImmutablePoint(50,50);
+    
+    private static final AlphaComposite ALPHA_COMPOSITE = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, .6f);
 
-    private final Map<Position, String> history = new HashMap<>();
-    private final Collection<Tile> placedTilesView;
+    private final LinkedList<PlacementHistoryEntry> entries = new LinkedList<PlacementHistoryEntry>();
+    private List<PlacementHistoryEntry> visibleEntries;
 
-    public PlacementHistory(GridPanel gridPanel, GameController gc, Collection<Tile> placedTilesView) {
+    public PlacementHistory(GridPanel gridPanel, GameController gc) {
         super(gridPanel, gc);
-        this.placedTilesView = placedTilesView;
+        
+        gc.register(this);
     }
-
-    public void update() {
-        history.clear();
-        int i = placedTilesView.size();
-        int limit = getGame().getAllPlayers().length;
-        for (Tile t : placedTilesView) {
-            if (i <= limit) {
-                history.put(t.getPosition(), "" + i);
-            }
-            i--;
-        }
+    
+    @Subscribe
+    public void handleTileEvent(TileEvent ev) {    	
+    	if (TileEvent.PLACEMENT == ev.getType()) {    		
+    		entries.addFirst(createPlacementHistoryEntry(ev));
+    	} else if (TileEvent.REMOVE == ev.getType()) {
+    		entries.remove(createPlacementHistoryEntry(ev));
+    	}
+    	
+    	int limit = 0;    	
+    	int ndx = 0;
+    	Player lastPlayer = null;
+    	Player previousPlayer = null;
+    	for (PlacementHistoryEntry entry : entries) {
+    		
+    		if (ndx == 0) {
+    			lastPlayer = previousPlayer = entry.player;
+    		}
+    		
+    		if (lastPlayer == null) {
+    			// player can be null (load, river-end)
+    			break;
+    		}
+    		
+    		if (previousPlayer != entry.player && lastPlayer == entry.player) {
+    			// complete round
+    			limit = ndx;
+    			break;
+    		}
+    		
+    		previousPlayer = entry.player;    		
+    		ndx++;
+    	}
+    	
+    	if (limit == 0) {
+    		limit = getGame().getAllPlayers().length;
+    	}
+    	
+    	visibleEntries = entries.subList(0, Math.min(entries.size(), limit));
     }
-
 
     @Override
     public void paint(Graphics2D g) {
-        for (Entry<Position, String> entry : history.entrySet()) {
-            drawAntialiasedTextCentered(g, entry.getValue(), 80, entry.getKey(), POINT, COLOR, null);
-        }
-
+    	
+    	if (visibleEntries == null) {
+    		return;
+    	}
+    	
+    	Composite oldComposite = g.getComposite();
+        g.setComposite(ALPHA_COMPOSITE);    	
+    	
+        int counter = 0;
+    	for (PlacementHistoryEntry entry : visibleEntries) {
+    		drawAntialiasedTextCentered(g, String.valueOf(++counter), 80, entry.position, POINT, 
+    				entry.player != null ?  entry.player.getColors().getFontColor() : DEFAULT_COLOR, null);
+    	}
+    	
+    	g.setComposite(oldComposite);
     }
+    
+    private PlacementHistoryEntry createPlacementHistoryEntry(TileEvent tileEvent) {
+    	return new PlacementHistoryEntry(tileEvent.getTriggeringPlayer(), tileEvent.getPosition());
+    }
+    
+	private class PlacementHistoryEntry {
+		private final Player player;
+		private final Position position;
+		
+		public PlacementHistoryEntry(Player player, Position position) {
+			this.player = player;
+			this.position = position;
+		}
+
+		private PlacementHistory getOuterType() {
+			return PlacementHistory.this;
+		}
+
+		@Override
+		public int hashCode() {
+			return com.google.common.base.Objects.hashCode(getOuterType(), player, position);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+
+			PlacementHistoryEntry that = (PlacementHistoryEntry) obj;
+
+			return com.google.common.base.Objects.equal(this.getOuterType(), that.getOuterType())
+					&& com.google.common.base.Objects.equal(this.player, that.player)
+					&& com.google.common.base.Objects.equal(this.position, that.position);
+		}
+
+	}
 }
