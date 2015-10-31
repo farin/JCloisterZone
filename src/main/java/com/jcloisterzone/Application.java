b62a34c9ebf1cd1358bf795af8510756052610ba
Roman Krejcik <farin@farin.cz>
2015-10-31 18:06:24 +0100

    align with release branch and unintentionally incresed protocol version

--- src/main/java/com/jcloisterzone/Application.java
+++ src/main/java/com/jcloisterzone/Application.java
@@ -6,15 +6,16 @@ package com.jcloisterzone;
  */
 public interface Application {
 
+	//use just for dev builds
     public String DEV_VERSION = "dev-snapshot";
 
-//    public String VERSION = "2.7-RC1";
-//    public String BUILD_DATE = "2014-06-26";
+//    public String VERSION = "X.Y.Z";
+//    public String BUILD_DATE = "YYYY-MM-DD";
 
     public String VERSION = DEV_VERSION;
     public String BUILD_DATE = "";
 
-    public String PROTCOL_VERSION = "3.4.0";
+    public String PROTCOL_VERSION = "3.4.1";
 
     public static final String ILLEGAL_STATE_MSG = "Method '{}' called in invalid state";
 }
