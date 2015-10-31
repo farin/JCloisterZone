c99dc052e6df87a3e8e4108a026436e3f7eb5cf0
Roman Krejcik <farin@farin.cz>
2015-10-31 15:18:34 +0100

    release 3.4.1

--- src/main/java/com/jcloisterzone/Application.java
+++ src/main/java/com/jcloisterzone/Application.java
@@ -8,10 +8,10 @@ public interface Application {
 
     public String DEV_VERSION = "dev-snapshot";
 
-    public String VERSION = "3.4.0";
-    public String BUILD_DATE = "2015-10-15";
+    public String VERSION = "3.4.1";
+    public String BUILD_DATE = "2015-10-31";
 
-    public String PROTCOL_VERSION = "3.4.0";
+    public String PROTCOL_VERSION = "3.4.1";
 
 
     public static final String ILLEGAL_STATE_MSG = "Method '{}' called in invalid state";
