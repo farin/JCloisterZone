988ffb1d4e6befa1ce2841c2e218aa8a272fa643
Roman Krejcik <farin@farin.cz>
2017-10-11 16:01:36 +0200

    AI ranking - added open features rating

--- src/main/java/com/jcloisterzone/ai/player/LegacyRanking.java
+++ src/main/java/com/jcloisterzone/ai/player/LegacyRanking.java
@@ -2,13 +2,36 @@ package com.jcloisterzone.ai.player;
 
 import com.jcloisterzone.Player;
 import com.jcloisterzone.ai.GameStateRanking;
+import com.jcloisterzone.feature.City;
+import com.jcloisterzone.feature.Cloister;
+import com.jcloisterzone.feature.Completable;
+import com.jcloisterzone.feature.Farm;
+import com.jcloisterzone.feature.Feature;
+import com.jcloisterzone.feature.Road;
+import com.jcloisterzone.feature.Scoreable;
 import com.jcloisterzone.figure.Follower;
 import com.jcloisterzone.figure.SmallFollower;
 import com.jcloisterzone.game.state.GameState;
 import com.jcloisterzone.game.state.PlacedTile;
 import com.jcloisterzone.game.state.PlayersState;
+import com.jcloisterzone.reducers.ScoreCompletable;
+
+import io.vavr.collection.Array;
+import io.vavr.collection.HashMap;
+import io.vavr.collection.Map;
+import io.vavr.collection.Seq;
+import io.vavr.collection.Stream;
 
 class LegacyRanking implements GameStateRanking {
+
+    public static final double[]  OPEN_ROAD_PENALTY =
+        { 0.0, 1.0, 2.5, 4.5, 7.5, 10.5, 14.5, 19.0, 29.0 };
+    public static final double[]  OPEN_CITY_PENALTY =
+        { 0.0, 0.5, 1.5, 3.0, 5.0, 8.0, 12.0, 17.0, 27.0 };
+    public static final double[]  OPEN_FARM_PENALTY =
+        { 0.0, 5.0, 10.0, 19.0, 28.0, 37.0, 47.0, 57.0, 67.0 };
+    public static final double[]  OPEN_CLOISTER_PENALTY =
+        { 0.0, 0.0, 0.4, 0.8, 1.2, 2.0, 4.0, 7.0, 11.0 };
 
     private final Player me;
 
@@ -29,6 +52,7 @@ class LegacyRanking implements GameStateRanking {
         lastPlaced = state.getLastPlaced();
 
         ranking += ratePoints();
+        ranking += rateUnfinishedFeatures();
         ranking += rateMeeples();
         ranking += rateBoardShape();
 
@@ -42,6 +66,59 @@ class LegacyRanking implements GameStateRanking {
             double q = player == me ? 1.0 : -1.0;
             r += q * ps.getScore().get(player.getIndex()).getPoints();
         }
+        return r;
+    }
+
+    private <T extends Scoreable> Stream<T> getOccupiedScoreables(GameState state, Class<T> cls) {
+        return state.getFeatures(cls).filter(c -> c.isOccupied(state));
+    }
+
+    private double rateUnfinishedFeatures() {
+        double r = 0.0;
+
+        for (Completable completable : getOccupiedScoreables(state, Completable.class)) {
+            ScoreCompletable sc = new ScoreCompletable(completable);
+            sc.apply(state); //no assign!
+            for (Player player : sc.getOwners()) {
+                double q = 1.0;
+                if (completable instanceof City) {
+                    q = 1.8;
+                }
+                if (player != me) {
+                    q = -q;
+                }
+                r += q * sc.getPoints();
+            }
+        }
+
+        Array<Seq<Follower>> followers = state.getPlayers().getFollowers();
+
+        @SuppressWarnings("rawtypes")
+        Map<Class, Integer> placedOn = followers
+            .get(me.getIndex())
+            .groupBy(f -> {
+                Feature feature = f.getFeature(state);
+                return (Class) (feature == null ? null : feature.getClass());
+            })
+            .mapValues(seq -> seq.size());
+
+        int open = placedOn.get(Road.class).getOrElse(0);
+        r -= OPEN_ROAD_PENALTY[open];
+        open = placedOn.get(City.class).getOrElse(0);
+        r -= OPEN_CITY_PENALTY[open];
+        open = placedOn.get(Cloister.class).getOrElse(0);
+        r -= OPEN_CLOISTER_PENALTY[open];
+        open = placedOn.get(Farm.class).getOrElse(0);
+        r -= OPEN_FARM_PENALTY[open];
+
+        for (Player player : state.getPlayers().getPlayers()) {
+            if (player == me) continue;
+            // penalty for followers in supply
+            r -= followers.get(player.getIndex())
+                .filter(f -> f.isInSupply(state))
+                .size();
+        }
+
         return r;
     }
 
