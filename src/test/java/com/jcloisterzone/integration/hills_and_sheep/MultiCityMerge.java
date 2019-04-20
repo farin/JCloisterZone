34e4311b8046fdff187403d484249e3dd9a2117d
Roman Krejcik <farin@farin.cz>
2019-04-20 21:44:32 +0200

    Add hills & sheep merge tests

---
+++ src/test/java/com/jcloisterzone/integration/hills_and_sheep/MultiCityMerge.java
@@ -0,0 +1,43 @@
+package com.jcloisterzone.integration.hills_and_sheep;
+
+import com.jcloisterzone.PlayerScore;
+import com.jcloisterzone.PointCategory;
+import com.jcloisterzone.game.state.GameState;
+import com.jcloisterzone.integration.IntegrationTest;
+import io.vavr.collection.Array;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MultiCityMerge extends IntegrationTest {
+
+    /**
+     *  HS.CC!.v merging
+     */
+    @Test
+    public void testMultiCityMerge() {
+        GameState state = createGameState("saved-games/hills_and_sheep/multi_city_merge.jcz");
+
+        Array<PlayerScore> score = state.getPlayers().getScore();
+        PlayerScore alice = score.get(0);
+        PlayerScore bob = score.get(1);
+
+        assertEquals(20, alice.getPoints());
+        assertEquals(0, bob.getPoints());
+    }
+
+    /**
+     *  HS.CC!.v merging
+     */
+    @Test
+    public void testMultiCityMerge2() {
+        GameState state = createGameState("saved-games/hills_and_sheep/multi_city_merge2.jcz");
+
+        Array<PlayerScore> score = state.getPlayers().getScore();
+        PlayerScore alice = score.get(0);
+        PlayerScore bob = score.get(1);
+
+        assertEquals(8, alice.getPoints());
+        assertEquals(8, bob.getPoints());
+    }
+}
