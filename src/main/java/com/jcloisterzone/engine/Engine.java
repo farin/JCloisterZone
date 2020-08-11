d7ac6a98975932d44b5ae69aee065d99606394e1
Roman Krejcik <farin@farin.cz>
2020-08-11 23:02:47 +0200

    initial seed message format

--- src/main/java/com/jcloisterzone/engine/Engine.java
+++ src/main/java/com/jcloisterzone/engine/Engine.java
@@ -204,7 +204,7 @@ public class Engine implements  Runnable {
 
         // initialSeed = -6476999185744582589L; // BA.C as first tile
         // initialSeed = 5898276208915289755L; // Princess and Dragon: early volcano + move, + also early princess (in general this provides early non BA tile, works also at least for Tower
-        err.println("initial seed is " + initialSeed);
+        err.println("%set initial-seed " + initialSeed);
 
         String line = in.nextLine();
         GameSetupMessage setupMsg = (GameSetupMessage) parser.fromJson(line);
