9a869f01de13bf74d7c7e45ec0313939276d9646
Roman Krejcik <farin@farin.cz>
2020-12-11 07:57:39 +0100

    rules: Shepherd can co-existent on farm with barn
    fixes #338

--- src/main/java/com/jcloisterzone/reducers/UndeployMeeples.java
+++ src/main/java/com/jcloisterzone/reducers/UndeployMeeples.java
@@ -9,6 +9,7 @@ import com.jcloisterzone.event.PlayEvent.PlayEventMeta;
 import com.jcloisterzone.feature.Feature;
 import com.jcloisterzone.figure.Barn;
 import com.jcloisterzone.figure.Meeple;
+import com.jcloisterzone.figure.Shepherd;
 import com.jcloisterzone.game.state.GameState;
 import com.jcloisterzone.game.state.NeutralFiguresState;
 import io.vavr.Tuple2;
@@ -19,7 +20,7 @@ import java.util.ArrayList;
 
 
 /**
- * Undeploys all meeples except Barn! Barns are never returned.
+ * Undeploys all meeples except Barn ans Shepherds! Barns are never returned and shepherds has special handling;
  */
 public class UndeployMeeples implements Reducer {
 
@@ -30,6 +31,7 @@ public class UndeployMeeples implements Reducer {
     public UndeployMeeples(Feature feature, boolean forced) {
         this.feature = feature;
         this.forced = forced;
+
     }
 
     @Override
@@ -42,7 +44,7 @@ public class UndeployMeeples implements Reducer {
         for (Tuple2<Meeple, FeaturePointer> t : state
                 .getDeployedMeeples()
                 .filter(t -> fps.contains(t._2))
-                .filter(t -> !(t._1 instanceof Barn))
+                .filter(t -> !(t._1 instanceof Barn) &&  !(t._1 instanceof Shepherd))
             ) {
             meeples.add(t._1);
             events.add(
