556faed233f7915093cf2769abb6311695e60bda
stanolacko <stano.lacko@gmail.com>
2021-11-23 15:06:20 +0100

    Prevention for MegicPortal and for currentPlaced tile to place Other than Smallfollower to Acrobats space

--- src/main/java/com/jcloisterzone/game/capability/AcrobatsCapability.java
+++ src/main/java/com/jcloisterzone/game/capability/AcrobatsCapability.java
@@ -3,6 +3,7 @@ package com.jcloisterzone.game.capability;
 import com.jcloisterzone.Player;
 import com.jcloisterzone.XMLUtils;
 import com.jcloisterzone.action.MeepleAction;
+import com.jcloisterzone.action.PlayerAction;
 import com.jcloisterzone.action.AcrobatsScoreAction;
 import com.jcloisterzone.board.Location;
 import com.jcloisterzone.board.Position;
@@ -12,23 +13,16 @@ import com.jcloisterzone.event.ExprItem;
 import com.jcloisterzone.event.PointsExpression;
 import com.jcloisterzone.event.ScoreEvent;
 import com.jcloisterzone.feature.Acrobats;
-import com.jcloisterzone.feature.Castle;
-import com.jcloisterzone.feature.Feature;
 import com.jcloisterzone.figure.Meeple;
 import com.jcloisterzone.figure.SmallFollower;
 import com.jcloisterzone.game.Capability;
-import com.jcloisterzone.game.capability.WatchtowerCapability.WatchtowerModifier;
 import com.jcloisterzone.game.state.ActionsState;
 import com.jcloisterzone.game.state.GameState;
 import com.jcloisterzone.game.state.PlacedTile;
-import com.jcloisterzone.io.message.AcrobatsScoreMessage;
 import com.jcloisterzone.reducers.AddPoints;
 import com.jcloisterzone.reducers.UndeployMeeples;
-import com.oracle.truffle.js.nodes.control.ReturnException;
 
-import io.vavr.Predicates;
 import io.vavr.Tuple2;
-import io.vavr.collection.HashMap;
 import io.vavr.collection.HashSet;
 import io.vavr.collection.LinkedHashMap;
 import io.vavr.collection.List;
@@ -64,13 +58,14 @@ public class AcrobatsCapability extends Capability<Void> {
     @Override
     public GameState onActionPhaseEntered(GameState state) {
         ActionsState actions = state.getPlayerActions();
-        HashSet places = HashSet.empty();
+        HashSet fullPlaces = HashSet.empty();
         Player active = state.getActivePlayer();
 
         PlacedTile lastPlaced = state.getLastPlaced();
         Position currentTilePos = lastPlaced.getPosition();
         
         Stream<Acrobats> acrobats = state.getFeatures(Acrobats.class);
+        List<FeaturePointer> acrobatsPlaces = List.empty();
         Vector<Meeple> availMeeples = active.getMeeplesFromSupply(state, Vector.of(SmallFollower.class));
 
         // Not allow to place Acrobat on tile with Bridge
@@ -83,8 +78,9 @@ public class AcrobatsCapability extends Capability<Void> {
             int count = state.getDeployedMeeples().filter((m, fp) -> {
             	return (acrobat.getPlaces().contains(fp));
             }).length();
+            acrobatsPlaces = acrobatsPlaces.appendAll(acrobat.getPlaces());
             if (count == ACROBATS_COUNT) {
-              places = places.union(acrobat.getPlaces().toSet());
+              fullPlaces = fullPlaces.union(acrobat.getPlaces().toSet());
             } else {
               for(Meeple meeple : availMeeples) {
             	  List<FeaturePointer> _places = acrobat.getPlaces().filter(fp -> {
@@ -98,10 +94,24 @@ public class AcrobatsCapability extends Capability<Void> {
               }
             }
         }
-        if (places.length()>0) {
-            actions = actions.appendAction(new AcrobatsScoreAction(places));
-            state = state.setPlayerActions(actions);
+        // Hack remove Acrobats placement to all other types of meeple
+        var currentActions = state.getPlayerActions().getActions();
+        List<FeaturePointer> _acrobatsPlaces = acrobatsPlaces;
+        Vector<PlayerAction<?>> filteredActions = currentActions.map(a -> {
+        			if (!(a instanceof MeepleAction) || ((MeepleAction) a).getMeepleType().equals(SmallFollower.class)) {
+        				return a;
+        			} else {
+        				MeepleAction ma = (MeepleAction) a;
+        		        ma = ma.excludeOptions(_acrobatsPlaces.toSet());
+        				return ma;
+        			}
+        		});
+        actions = actions.setActions(filteredActions);
+		state = state.setPlayerActions(actions);
 
+        if (fullPlaces.length()>0) {
+            actions = actions.appendAction(new AcrobatsScoreAction(fullPlaces));
+            state = state.setPlayerActions(actions);
         }
         return state;
     }
