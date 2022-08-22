f5b9a7b4a1b0ee234442bb5cedbbae4c227b968b
stanolacko <stano.lacko@gmail.com>
2022-08-22 09:05:41 +0200

    Change logic for onTileRule for Fairy a Acrobats

--- src/main/java/com/jcloisterzone/game/phase/FairyPhase.java
+++ src/main/java/com/jcloisterzone/game/phase/FairyPhase.java
@@ -31,15 +31,28 @@ public class FairyPhase extends Phase {
 
         boolean onTileRule = ptr instanceof Position;
         FeaturePointer fairyFp = ptr.asFeaturePointer();
-
-        for (Tuple2<Meeple, FeaturePointer> t : state.getDeployedMeeples()) {
+        GameState _state = state;
+        
+        Set<Tuple2<Meeple, FeaturePointer>> playerMeeplesOnTileWithFairy = state.getDeployedMeeples()
+        		.filter(t -> t._1.getPlayer().equals(_state.getTurnPlayer()))
+        		.filter(t -> t._2.getPosition().equals(fairyFp.getPosition()))
+        		.toSet();
+        
+        boolean isAcrobats = !playerMeeplesOnTileWithFairy
+        		.filter(t -> t._1.getFeature(_state) instanceof Acrobats)
+        		.isEmpty();
+        
+        for (Tuple2<Meeple, FeaturePointer> t : playerMeeplesOnTileWithFairy) {
             Meeple m = t._1;
-            if (!m.getPlayer().equals(state.getTurnPlayer())) continue;
             if (onTileRule) {
-                if (!t._2.getPosition().equals(fairyFp.getPosition())) continue;
-            } else if (!(m.getFeature(state) instanceof Acrobats)) {
-                if (!t._2.equals(fairyFp)) continue;
-               	if (!((MeeplePointer) ptr).match(m)) continue;
+            	if (isAcrobats && !(m.getFeature(state) instanceof Acrobats)) {
+            		continue;
+            	}
+            } else {
+        		if (!t._2.equals(fairyFp)) continue;
+            	if (!(m.getFeature(state) instanceof Acrobats)) {
+            		if (!((MeeplePointer) ptr).match(m)) continue;
+            	}
             }
 
             PointsExpression expr = new PointsExpression("fairy.turn", new ExprItem("fairy", FairyCapability.FAIRY_POINTS_BEGINNING_OF_TURN));
@@ -51,4 +64,4 @@ public class FairyPhase extends Phase {
 
         return next(state);
     }
-}
+ }
